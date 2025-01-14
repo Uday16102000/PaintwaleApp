ff2271f6b209f97e3fe74c835447401bdf022fb4
Akash <akash@aapkapainter.com>
2025-01-14 08:23:22 +0530

    api models changes

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from paintwaleapp.models import *
 
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'active']
+    search_fields = ['username', 'email']
+    ordering = ['username']
+    
 # Register your models here.
-admin.site.register(Users)
+admin.site.register(Users, CustomUserAdmin)
+admin.site.register(City)
--- paintwaleapp/models.py
+++ paintwaleapp/models.py
@@ -2,116 +2,8 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.utils.text import slugify
-import random,string
-
-# class AuthGroup(models.Model):
-#     name = models.CharField(unique=True, max_length=150)
-
-#     class Meta:
-#         managed = False
-#         db_table = 'auth_group'
-
-
-# class AuthGroupPermissions(models.Model):
-#     group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-#     permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-#     class Meta:
-#         managed = False
-#         db_table = 'auth_group_permissions'
-#         unique_together = (('group', 'permission'),)
-
-
-# class AuthPermission(models.Model):
-#     content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-#     codename = models.CharField(max_length=100)
-#     name = models.CharField(max_length=255)
-
-#     class Meta:
-#         managed = False
-#         db_table = 'auth_permission'
-#         unique_together = (('content_type', 'codename'),)
-
-
-# class AuthUser(models.Model):
-#     password = models.CharField(max_length=128)
-#     last_login = models.DateTimeField(blank=True, null=True)
-#     is_superuser = models.BooleanField()
-#     username = models.CharField(unique=True, max_length=150)
-#     last_name = models.CharField(max_length=150)
-#     email = models.CharField(max_length=254)
-#     is_staff = models.BooleanField()
-#     is_active = models.BooleanField()
-#     date_joined = models.DateTimeField()
-#     first_name = models.CharField(max_length=150)
-
-#     class Meta:
-#         managed = False
-#         db_table = 'auth_user'
-
-
-# class AuthUserGroups(models.Model):
-#     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-#     group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-#     class Meta:
-#         managed = False
-#         db_table = 'auth_user_groups'
-#         unique_together = (('user', 'group'),)
-
-
-# class AuthUserUserPermissions(models.Model):
-#     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-#     permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-#     class Meta:
-#         managed = False
-#         db_table = 'auth_user_user_permissions'
-#         unique_together = (('user', 'permission'),)
-
-
-# class DjangoAdminLog(models.Model):
-#     object_id = models.TextField(blank=True, null=True)
-#     object_repr = models.CharField(max_length=200)
-#     action_flag = models.PositiveSmallIntegerField()
-#     change_message = models.TextField()
-#     content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-#     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-#     action_time = models.DateTimeField()
-
-#     class Meta:
-#         managed = False
-#         db_table = 'django_admin_log'
-
-
-# class DjangoContentType(models.Model):
-#     app_label = models.CharField(max_length=100)
-#     model = models.CharField(max_length=100)
-
-#     class Meta:
-#         managed = False
-#         db_table = 'django_content_type'
-#         unique_together = (('app_label', 'model'),)
-
-
-# class DjangoMigrations(models.Model):
-#     app = models.CharField(max_length=255)
-#     name = models.CharField(max_length=255)
-#     applied = models.DateTimeField()
-
-#     class Meta:
-#         managed = False
-#         db_table = 'django_migrations'
-
-
-# class DjangoSession(models.Model):
-#     session_key = models.CharField(primary_key=True, max_length=40)
-#     session_data = models.TextField()
-#     expire_date = models.DateTimeField()
-
-#     class Meta:
-#         managed = False
-#         db_table = 'django_session'
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.hashers import make_password, check_password
 
 class Service(models.Model):
     service_name = models.CharField(max_length=255, unique=True)
@@ -140,6 +32,21 @@ class ServiceImage(models.Model):
         managed = True
         db_table = 'service_image'
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+    
 def generate_unique_username(name, model_class):
     base_username = slugify(name)[:20]  # Slugify and limit to 20 chars
     username = base_username
@@ -155,13 +62,36 @@ class Users(models.Model):
     username = models.CharField(max_length=100,blank=False, null=False,unique=True)
     phone = models.CharField(max_length=20,blank=True, null=True,default=None)
     otp = models.IntegerField(blank=True, null=True,default=None)
-    email_id = models.CharField(max_length=50,blank=True, null=True,default=None)
+    email = models.CharField(max_length=50,blank=True, null=True,default=None)
+    password = models.CharField(max_length=255,blank=True, null=True,default=None)
     active = models.BooleanField(blank=True, null=True, default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now=True)
     
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['email','password']
     
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.username
+    
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+    
+    def has_perm(self, perm, obj=None):
+        if self.is_superuser:
+            return True
+        return False
+
+    def has_module_perms(self, app_label):
+        if self.is_superuser:
+            return True
+        return False
     @property
     def is_authenticated(self):
         return True
@@ -237,3 +167,32 @@ class Walls(models.Model):
     class Meta:
         managed = True
         db_table = 'walls'
+
+class City(models.Model):
+    city_name = models.CharField(max_length=100,blank=True, null=True, default=None)
+    city_code = models.CharField(max_length=20,blank=True, null=True, default=None)
+    active = models.BooleanField(blank=True, null=True, default=True)
+    
+    class Meta:
+        managed = True
+        db_table = 'city'
+
+class PriceList(models.Model):
+    process = models.ForeignKey(Process, models.DO_NOTHING, db_column='process_id')
+    product = models.ForeignKey(Product, models.DO_NOTHING, db_column='product_id')
+    city = models.ForeignKey(City, models.DO_NOTHING,db_column='city_id')
+    price = models.FloatField(blank=True, null=True, default=None)
+    is_lumpsum_rate = models.IntegerField(blank=True, null=True,default=0) # 0: No, 1: Yes
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(Users, models.DO_NOTHING,db_column='created_by',related_name='createdpricelist')
+    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(Users, models.DO_NOTHING,db_column='updated_by',related_name='updatedpricelist')
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['process','product','city'],name='unique_process_product_city')
+        ]
+        managed = True
+        db_table = 'price_list'
+    
+    
